c52faf8139b3d4e62aaa15a128b1f8117201dd03
Alex Zaporozhsky <zaporozhsky@yandex.ru>
2023-11-18 18:49:21 +0300

    second commit

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,11 +1,18 @@
 //icons can be found at https://loading.io/
 
 import React from "react";
+import Image from "next/image";
 
 export default function loading() {
 	return (
 		<div className="flex justify-center">
-			<img src="loading.svg" alt="loading..." className="h-16" />
+			<Image
+				src="loading.svg"
+				alt="loading..."
+				className="h-16"
+				width="100%"
+				height="100%"
+			/>
 		</div>
 	);
 }
